f8ac93fd35b44967d727be9215d34db7b908ebe3
shubhampawar4841 <shubhampawar4036@gmail.com>
2024-11-09 18:04:55 +0530

    lets start

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,14 @@
+const { LiaTerminalSolid } = require("react-icons/lia");
+
 console.log("hello");
+
+// Compiler convert high level developer code into 0s and 1s
+// Javascript is Interpreted code so it doesnt have compilation time
+/
+
+//Compiled Language => first need to compile anf then run and usallay dont compile if there is a error in code
+Example => C++, Java, Rust, Golang
+
+//Interpreted Language => Usually Line to line 
+// can run partially if error comes later 
+Example Javascript Python
